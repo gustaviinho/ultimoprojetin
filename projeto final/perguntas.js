c2627b5c69b77ead44646199cd6cbcb1d02011c6
gustaviinho <guto.31almeidacosta@gmail.com>
2024-11-29 10:47:28 -0300

    Add files via upload

---
+++ projeto final/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'musica',
+    'O que é HIP HOP',
+    'E um conjunto cultural onde envolve musica(rap), pinturas(grafite) e dança(break)'
+)
+
+criaCartao(
+    'rap',
+    'os 4 pilares do HIP HOP',
+    'As musicas, as danças, as artes e os beatbox'
+)
+
+criaCartao(
+    'rap',
+    'O que o HIP HOP defende',
+    'A música rap apresenta características  capaz de expressar os sentimentos de revolta e exclusão de jovens marginalizados em todo o mundo: negros e latinos nos Estados Unidos, turcos na Alemanha, pobres e submetidos à violência e ao racismo na América Latina'
+)
+
+criaCartao(
+    'musica',
+    'O que essas musicas representam ',
+    'elas representa crenças ou tradições de algum lugar, ou apenas retratar tudo o que se passa em um certo lugar, um pensamento, uma forma de se expresar'
+)
